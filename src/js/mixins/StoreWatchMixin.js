0046bc1cf82d3acd4e1e59af0b0bf406017dc3cb
Teddy NGUYEN <chauminh88@hotmail.com>
2017-04-16 21:00:49 +0700

    Refactored to mixin and start organizing project structure

--- src/js/mixins/StoreWatchMixin.js
+++ src/js/mixins/StoreWatchMixin.js
@@ -2,21 +2,25 @@ import React from 'react';
 import AppStore from '../stores/app-store';
 
 export default ( InnerComponent, stateCallback) => class extends React.Component {
-    // constructor(props) {
-    //     super();
-    //     this.state = cartItems();
-    //     this._onChange = this._onChange.bind(this);
-    // }
+    constructor(props) {
+        super( props );
+        this.state = stateCallback( props );
+        this._onChange = this._onChange.bind(this);
+    }
 
-    // componentWillMount() {
-    //     AppStore.addChangeListener( this._onChange );
-    // }
+    componentWillMount() {
+        AppStore.addChangeListener( this._onChange );
+    }
 
-    // componentWillUnmount() {
-    //     AppStore.removeChangeListener( this._onChange );
-    // }
+    componentWillUnmount() {
+        AppStore.removeChangeListener( this._onChange );
+    }
 
-    // _onChange() {
-    //     this.setState( cartItems() )
-    // }
+    _onChange() {
+        this.setState( stateCallback( this.props ) )
+    }
+
+    render() {
+        return <InnerComponent {...this.state} {...this.props} />
+    }
 } 
