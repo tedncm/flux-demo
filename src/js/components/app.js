6b35c884489571095b421af8b294ebed1d623c5e
Teddy NGUYEN <chauminh88@hotmail.com>
2017-04-16 18:56:23 +0700

    Done with Catalog Item render in home page

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppActions from '../actions/app-actions';
+import Catalog from './app-catalog';
 
 export default class App extends React.Component {
     render() {
-        return <h1 onClick={ AppActions.addItem.bind(null, 'This is an item')} >Hello FLUXY</h1>
+        return (
+        <div className="container">
+            <Catalog />
+        </div>
+        )
     } 
 }
